71fe0360eaa45ad3cb9161ee3a0766cf3b5e79e2
JinYana <ngjinyang@gmail.com>
2021-01-18 23:06:06 +0800

    Add project files.

---
+++ PRG2 Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PRG2_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
