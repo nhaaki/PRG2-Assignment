456fce36838519a4e68efd0bb01fe78fb967b718
Jin Yang Ng <s10203386@connect.np.edu.sg>
2021-01-19 17:09:14 +0800

    Added person class(unfinished)

---
+++ PRG2 Assignment/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRG2_Assignment
+{
+    class Person
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        
+    }
+}
