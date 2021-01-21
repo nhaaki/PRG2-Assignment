60c424ddd4314909b25434eb86a3f392faf6c056
JinYana <ngjinyang@gmail.com>
2021-01-21 21:58:48 +0800

    Added API class(for SHNFacility data usage)

---
+++ PRG2 Assignment/API.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRG2_Assignment
+{
+    class API
+    {
+        public string facilityname { get; set; }
+        public int facilitycapacity { get; set; }
+        public double distfromaircheckpoint { get; set; }
+        public double distfromseacheckpoint { get; set; }
+        public double distfromlandcheckpoint { get; set; }
+
+
+        public API() { }
+    }
+}
