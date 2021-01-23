2f5dd8ccb2e898affd515565834a955bc20c6a90
JinYana <ngjinyang@gmail.com>
2021-01-23 16:18:35 +0800

    Loaded Both CSV files, loaded API, Listed Visitors

--- PRG2 Assignment/Program.cs
+++ PRG2 Assignment/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace PRG2_Assignment
 {
@@ -6,7 +11,68 @@ namespace PRG2_Assignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string[] personLines = File.ReadAllLines("Person.csv");
+
+            string[] BusinessLines = File.ReadAllLines("BusinessLocation.csv");
+
+            List<API> apidata = LoadAPI();
+
+            ListVisitors(personLines);
+
+
+
         }
+
+        
+
+        static List<API> LoadAPI()
+        {
+            List<API> apiList = new List<API>();
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://covidmonitoringapiprg2.azurewebsites.net");
+                Task<HttpResponseMessage> responseTask = client.GetAsync("/facility");
+                responseTask.Wait();
+                HttpResponseMessage result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    Task<string> apiTask = result.Content.ReadAsStringAsync();
+                    apiTask.Wait();
+                    string data = apiTask.Result;
+                    apiList = JsonConvert.DeserializeObject<List<API>>(data);
+                    return apiList;
+                }
+                else
+                {
+                    return apiList;
+                }
+            }
+        }
+
+        static void ListVisitors(string[] lines)
+        {
+            Console.WriteLine("Visitors");
+            Console.WriteLine("--------");
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] person = lines[i].Split(',');
+                if (person[0] == "visitor")
+                {
+
+                    Console.WriteLine(person[1]);
+
+                }
+            }
+        }
+
+        
+
     }
+
+
+
+    
 }
+
+
